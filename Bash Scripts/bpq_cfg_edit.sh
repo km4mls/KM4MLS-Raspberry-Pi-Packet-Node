261512d75c5206136ad4e69448135a15a53367d6
KM4MLS <31637008+km4mls@users.noreply.github.com>
2017-09-09 00:46:01 -0400

    update bpq_cfg_edit.sh

--- Bash Scripts/bpq_cfg_edit.sh
+++ Bash Scripts/bpq_cfg_edit.sh
@@ -8,14 +8,14 @@
 # Description:  This script modifies the BPQ32.cfg starter file supplied
 #               in the KM4MLS-Raspberry-Pi-Packet-Node github repository.
 #               It is designed to make getting LINBPQ (BPQ32) set-up
-#               reletively easy and strait-forward. 
+#               reletively easy and strait-forward.
 #
 #               The script asks the user for his/her callsign and other
 #               relevent information related to the config of BPQ, then 
 #               replaces the parts of the BPQ32.cfg that must be unique
 #               for each node.
 #
-# Notes:        This script is currently in the alpha stage. It does not 
+# Notes:        This script is currently in the alpha stage. It does not
 #               edit or modify any system files, but with that said, run
 #               at your own risk.
 
